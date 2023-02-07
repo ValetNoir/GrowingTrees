7525cb9eb22ce07602615403e8f04e5035dfe0c4
valet noir <valetnoir.gaming@gmail.com>
2023-02-07 17:55:13 +0100

    implemented part one of the bubble algorithm

--- js/bubbles.js
+++ js/bubbles.js
@@ -1,8 +1,3 @@
-const circles = [
-  {center: {x: 0.0, y: 0.0}, radius: 1.2},
-  {center: {x: 1.0, y: 0.1}, radius: 1.2},
-];
-
 function getIntersectionPoints(c1, c2) {
   // after 5 hours of struggling to solve an equation, used the internet
   // https://www.petercollingridge.co.uk/tutorials/computational-geometry/circle-circle-intersections/
@@ -12,10 +7,10 @@ function getIntersectionPoints(c1, c2) {
   const d = Math.sqrt(dx * dx + dy * dy);
 
   // Circles too far apart
-  if (d > c1.radius + c2.radius) { return; }
+  if (d > c1.radius + c2.radius) { return []; }
 
   // One circle completely inside the other
-  if (d < Math.abs(c1.radius - c2.radius)) { return; }
+  if (d < Math.abs(c1.radius - c2.radius)) { return []; }
 
   dx /= d;
   dy /= d;
--- js/main.js
+++ js/main.js
@@ -1,30 +1,34 @@
+const circles = [
+  {center: {x: 0, y: 0}, radius: 350},
+  {center: {x: 500, y: 5}, radius: 200},
+  {center: {x: 300, y: 200}, radius: 300},
+  {center: {x: -200, y: -100}, radius: 150},
+];
 
-import * as THREE from 'three';
-import * as bubbles from 'bubbles';
-  
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+ctx.translate(1500, 750);
 
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-camera.position.set( 0, 0, 100 );
-camera.lookAt( 0, 0, 0 );
+draw();
+function draw() {
+  ctx.lineWidth = 3;
+  for(let i = 0; i < circles.length; i++) {
+    ctx.strokeStyle = 'black';
+    ctx.beginPath();
+    ctx.arc(circles[i].center.x, circles[i].center.y, circles[i].radius, 0, 2 * Math.PI);
+    ctx.stroke();
 
-const scene = new THREE.Scene();
-
-const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
-
-const points = bubbles.getIntersectionPoints(bubbles.circles[0], bubbles.circles[1]);
-
-// const points = [];
-// points.push( new THREE.Vector3( - 10, 0, 0 ) );
-// points.push( new THREE.Vector3( 0, 10, 0 ) );
-// points.push( new THREE.Vector3( 10, 0, 0 ) );
-// points.push( new THREE.Vector3( - 10, 0, 0 ) );
-
-const geometry = new THREE.BufferGeometry().setFromPoints( points );
-
-const line = new THREE.Line( geometry, material );
-
-scene.add( line );
-renderer.render( scene, camera );
+    ctx.strokeStyle = 'blue';
+    for(let j = 0; j < circles.length; j++) {
+      const points = getIntersectionPoints(circles[i], circles[j]);
+      let l = points.length;
+      if(l == 0) continue;
+      ctx.beginPath();
+      ctx.moveTo(points[0].x, points[0].y);
+      for(let i2 = 1; i2 < points.length; i2++) {
+        ctx.lineTo(points[i2].x, points[i2].y);
+      }
+      ctx.stroke();
+    }
+  }
+}
